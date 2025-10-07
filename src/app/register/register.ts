af3fdca8f64bdf82b115ff89a61c5dc71968d5a1
alfaiz-exe <alfaizkhan407@gmail.com>
2025-10-07 21:07:05 +0530

    update

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,19 +1,55 @@
-import { Component } from '@angular/core';
+import { inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { User } from '../../services/user'
+import { CookieService } from 'ngx-cookie-service';
+import { TuiTextfieldComponent } from "@taiga-ui/core";
+import {AsyncPipe, NgIf} from '@angular/common';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {
+    TuiAppearance,
+    TuiButton,
+    TuiError,
+    TuiIcon,
+    TuiNotification,
+    TuiTextfield,
+    TuiTitle,
+} from '@taiga-ui/core';
+import {TuiFieldErrorPipe, TuiSegmented, TuiSwitch, TuiTooltip} from '@taiga-ui/kit';
+import {TuiCardLarge, TuiForm, TuiHeader} from '@taiga-ui/layout';
+ 
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,
+      TuiTextfield,
+       AsyncPipe,
+        ReactiveFormsModule,
+        TuiAppearance,
+        TuiButton,
+        TuiCardLarge,
+        TuiError,
+        TuiFieldErrorPipe,
+        TuiForm,
+        TuiHeader,
+        TuiNotification,
+        TuiSegmented,
+        TuiSwitch,
+        TuiTextfield,
+        TuiTitle,
+        TuiTooltip
+  ],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
-  username: string = '';
-  password: string = '';
+  protected readonly form = new FormGroup({
+      username:new FormControl(''),
+      password:new FormControl('')
+  })
   constructor(private http: HttpClient, private userservice: User, private router: Router) {
 
   }
@@ -22,8 +58,8 @@ export class Register {
   }
   onSubmit() {
     const payload = {
-      username: this.username,
-      password: this.password
+      username: this.form.value.username,
+      password: this.form.value.password
     };
     console.log(payload)
     this.http.post<any>('http://localhost:3000/auth/register', payload)

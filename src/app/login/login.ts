0a08dffeff37820ba406e9c05c0feb033625979f
alfaiz-exe <alfaizkhan407@gmail.com>
2025-10-07 19:58:41 +0530

    commit

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,17 +1,51 @@
-import { Component, inject } from '@angular/core';
+import { inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { User } from '../../services/user'
 import { CookieService } from 'ngx-cookie-service';
+import { TuiTextfieldComponent } from "@taiga-ui/core";
+import {AsyncPipe, NgIf} from '@angular/common';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {
+    TuiAppearance,
+    TuiButton,
+    TuiError,
+    TuiIcon,
+    TuiNotification,
+    TuiTextfield,
+    TuiTitle,
+} from '@taiga-ui/core';
+import {TuiFieldErrorPipe, TuiSegmented, TuiSwitch, TuiTooltip} from '@taiga-ui/kit';
+import {TuiCardLarge, TuiForm, TuiHeader} from '@taiga-ui/layout';
+ 
+
 interface ApiRes {
-  username: string,
-  token: string
-};
-@Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [FormsModule],
+    username: string,
+    token: string
+  };
+  @Component({
+    selector: 'app-login',
+    standalone: true,
+    imports: [FormsModule,
+      TuiTextfield,
+       AsyncPipe,
+        ReactiveFormsModule,
+        TuiAppearance,
+        TuiButton,
+        TuiCardLarge,
+        TuiError,
+        TuiFieldErrorPipe,
+        TuiForm,
+        TuiHeader,
+        TuiNotification,
+        TuiSegmented,
+        TuiSwitch,
+        TuiTextfield,
+        TuiTitle,
+        TuiTooltip
+  ],
   providers: [CookieService],
   templateUrl: './login.html',
   styleUrl: './login.css'
@@ -19,6 +53,10 @@ interface ApiRes {
 export class Login {
   username: string = '';
   password: string = '';
+  protected readonly form = new FormGroup({
+    username:new FormControl(this.username),
+    password:new FormControl(this.password)
+  })
   cookieService = inject(CookieService);
   constructor(private http: HttpClient, private userservice: User, private router: Router) {
 
